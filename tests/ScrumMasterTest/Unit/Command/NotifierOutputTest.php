fb8557252bd110f00aab3a19a165ea717e3c5a96
Chemaclass <chemaclass@outlook.es>
2019-11-20 10:57:22 +0100

    Improve NotifierOutputTest

--- tests/ScrumMasterTest/Unit/Command/NotifierOutputTest.php
+++ tests/ScrumMasterTest/Unit/Command/NotifierOutputTest.php
@@ -15,21 +15,19 @@ final class NotifierOutputTest extends TestCase
     public function writeFromSlackNotifierOutput(): void
     {
         $result = new SlackNotifierResult();
-        $result->addChannelIssue('KEY-1', ChannelIssue::withStatusCode(100));
-        $result->addChannelIssue('KEY-2', ChannelIssue::withCodeAndAssignee(200, 'jira.user.1'));
-        $result->addChannelIssue('KEY-3', ChannelIssue::withStatusCode(300));
-        $result->addChannelIssue('KEY-4', ChannelIssue::withCodeAndAssignee(100, 'jira.user.2'));
-        $result->addChannelIssue('KEY-5', ChannelIssue::withCodeAndAssignee(100, 'jira.user.1'));
+        $result->addChannelIssue('K-1', ChannelIssue::withStatusCode(100));
+        $result->addChannelIssue('K-2', ChannelIssue::withCodeAndAssignee(200, 'j.user.1'));
+        $result->addChannelIssue('K-3', ChannelIssue::withStatusCode(300));
+        $result->addChannelIssue('K-4', ChannelIssue::withCodeAndAssignee(100, 'j.user.2'));
+        $result->addChannelIssue('K-5', ChannelIssue::withCodeAndAssignee(100, 'j.user.1'));
 
-        $output = new InMemoryOutput();
-        (new NotifierOutput($output))->write(['any channel name' => $result]);
+        $inMemoryOutput = new InMemoryOutput();
+        (new NotifierOutput($inMemoryOutput))->write(['any channel name' => $result]);
 
-        $this->assertContains('# CHANNEL: any channel name', $output->lines());
-        $this->assertContains('Total successful notifications sent: 1 (KEY-2)', $output->lines());
-        $this->assertContains('Total failed notifications sent: 4 (KEY-1, KEY-3, KEY-4, KEY-5)', $output->lines());
-        $this->assertContains(
-            'Total notifications: 5 (KEY-1, KEY-2: jira.user.1, KEY-3, KEY-4: jira.user.2, KEY-5: jira.user.1)',
-            $output->lines()
-        );
+        $lines = $inMemoryOutput->lines();
+        $this->assertContains('# CHANNEL: any channel name', $lines);
+        $this->assertContains('Total notifications: 5 (K-1, K-2: j.user.1, K-3, K-4: j.user.2, K-5: j.user.1)', $lines);
+        $this->assertContains('Total successful notifications sent: 1 (K-2)', $lines);
+        $this->assertContains('Total failed notifications sent: 4 (K-1, K-3, K-4, K-5)', $lines);
     }
 }
