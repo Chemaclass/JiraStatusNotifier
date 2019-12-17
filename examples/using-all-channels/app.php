22ccd37e536d1b963d70fc5fe7a8a8f5bfebe354
Chemaclass <chemaclass@outlook.es>
2019-12-17 10:54:25 +0100

    Added OVERRIDDEN_EMAILS to examples/using-all-channels

--- examples/using-all-channels/app.php
+++ examples/using-all-channels/app.php
@@ -51,7 +51,8 @@ $channels = [
         new Mailer(new GmailSmtpTransport(getenv('MAILER_USERNAME'), getenv('MAILER_PASSWORD'))),
         Email\MessageGenerator::withTimeToDiff(new DateTimeImmutable()),
         new Email\AddressGenerator((new ByPassEmail())
-            ->setSendEmailsToAssignee(false)
+            ->setSendEmailsToAssignee(false) // <- OverriddenEmails will have no effect as long as this is false
+            ->setOverriddenEmails(json_decode($_ENV['OVERRIDDEN_EMAILS'], true))
             ->setSendCopyTo(getenv('MAILER_USERNAME')))
     ),
     new Slack\Channel(
