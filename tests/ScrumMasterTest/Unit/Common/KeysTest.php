9354c66d634739a434dfdbb8b7eba0a4d485ef28
Chemaclass <chemaclass@outlook.es>
2019-12-20 19:18:13 +0100

    Create Common\Keys::fromEnvFile()

---
+++ tests/ScrumMasterTest/Unit/Common/KeysTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chemaclass\ScrumMasterTests\Unit\Common;
+
+use Chemaclass\ScrumMaster\Common\Keys;
+use PHPUnit\Framework\TestCase;
+
+final class KeysTest extends TestCase
+{
+    /** @test */
+    public function fromEnvFile(): void
+    {
+        $content = <<<ENV
+COMPANY_NAME=~
+JIRA_USERS_TO_IGNORE='[]'
+# Email Channel
+MAILER_USERNAME=~
+OVERRIDDEN_EMAILS='[]'
+
+ENV;
+        self::assertEquals([
+            'COMPANY_NAME',
+            'JIRA_USERS_TO_IGNORE',
+            'MAILER_USERNAME',
+            'OVERRIDDEN_EMAILS',
+        ], Keys::fromEnvFile($content));
+    }
+}
