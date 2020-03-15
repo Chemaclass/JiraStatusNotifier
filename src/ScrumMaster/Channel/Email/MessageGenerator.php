f63ffc0e1f4deec41c6ef1b1ea350a5ab30ecee7
Chemaclass <chemaclass@outlook.es>
2020-03-15 14:00:14 +0100

    Create email-template.twig and use it in the email-channel example

--- src/ScrumMaster/Channel/Email/MessageGenerator.php
+++ src/ScrumMaster/Channel/Email/MessageGenerator.php
@@ -5,63 +5,29 @@ declare(strict_types=1);
 namespace Chemaclass\ScrumMaster\Channel\Email;
 
 use Chemaclass\ScrumMaster\Channel\MessageGeneratorInterface;
-use Chemaclass\ScrumMaster\Jira\ReadModel\Assignee;
-use Chemaclass\ScrumMaster\Jira\ReadModel\JiraTicket;
 use DateTimeImmutable;
+use Twig;
 
 final class MessageGenerator implements MessageGeneratorInterface
 {
     private DateTimeImmutable $now;
 
-    public static function beingNow(DateTimeImmutable $now): self
-    {
-        return new self($now);
-    }
+    private Twig\Environment $twig;
 
-    private function __construct(DateTimeImmutable $now)
+    public function __construct(DateTimeImmutable $now, Twig\Environment $twig)
     {
         $this->now = $now;
+        $this->twig = $twig;
     }
 
     public function forJiraTickets(array $tickets, string $companyName): string
     {
-        /** @var Assignee $assignee */
-        $assignee = $tickets[array_key_first($tickets)]->assignee();
-        $text = $this->headerText($assignee);
         uksort($tickets, 'strnatcasecmp');
 
-        foreach ($tickets as $ticket) {
-            $text .= $this->textForTicket($ticket, $companyName);
-        }
-
-        return $text;
-    }
-
-    private function textForTicket(JiraTicket $ticket, string $companyName): string
-    {
-        $status = $ticket->status();
-        $daysDiff = $status->changeDate()->diff($this->now)->days;
-        $url = "https://{$companyName}.atlassian.net/browse/{$ticket->key()}";
-        $dayWord = ($daysDiff > 1) ? 'days' : 'day';
-
-        return <<<TXT
-<div class="ticket">
-    <b>Jira Ticket</b>: <a href="{$url}">{$ticket->key()}</a> - <i>{$ticket->title()}</i> <br>
-    <b>Current status</b>: <i>{$status->name()}</i> since {$daysDiff} {$dayWord}<br>
-    <b>Story Points</b>: {$ticket->storyPoints()}<br>
-</div>
-<hr>
-TXT;
-    }
-
-    private function headerText(Assignee $assignee): string
-    {
-        if ($assignee->key()) {
-            $salutation = "Hey, {$assignee->displayName()} ({$assignee->name()})";
-        } else {
-            $salutation = 'Hey Team';
-        }
-
-        return '<header>' . $salutation . ', please have a look:</header><hr>';
+        return $this->twig->render('email-template.twig', [
+            'tickets' => $tickets,
+            'now' => $this->now,
+            'companyName' => $companyName,
+        ]);
     }
 }
