4bebcb973eb0c0fca029d42bf1fee168c56903a9
Chemaclass <chemaclass@outlook.es>
2020-03-21 12:45:25 +0100

    Assert ticket types from email message generator

--- src/ScrumMaster/Channel/Email/MessageGenerator.php
+++ src/ScrumMaster/Channel/Email/MessageGenerator.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace Chemaclass\ScrumMaster\Channel\Email;
 
 use Chemaclass\ScrumMaster\Channel\MessageGeneratorInterface;
+use Chemaclass\ScrumMaster\Jira\ReadModel\JiraTicket;
 use DateTimeImmutable;
 use Twig;
+use Webmozart\Assert\Assert;
 
 final class MessageGenerator implements MessageGeneratorInterface
 {
+    private const TEMPLATE_NAME = 'email-template.twig';
+
     private DateTimeImmutable $now;
 
     private Twig\Environment $twig;
@@ -22,12 +26,16 @@ final class MessageGenerator implements MessageGeneratorInterface
 
     public function forJiraTickets(array $tickets, string $companyName): string
     {
+        Assert::allIsInstanceOf($tickets, JiraTicket::class);
         uksort($tickets, 'strnatcasecmp');
 
-        return $this->twig->render('email-template.twig', [
-            'tickets' => $tickets,
-            'now' => $this->now,
-            'companyName' => $companyName,
-        ]);
+        return $this->twig->render(
+            self::TEMPLATE_NAME,
+            [
+                'tickets' => $tickets,
+                'now' => $this->now,
+                'companyName' => $companyName,
+            ]
+        );
     }
 }
